1fb4bc4fa23a54e773ccddf64a92c65767d19d06
Matas Pieczulis <mateuszpieczulis123@outlook.com>
2023-08-01 11:39:18 +0200

    added memory test

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,8 +1,19 @@
 import pytest 
+import os, psutil
+from modules.game import Game
 
 def test_fps():
-    from modules.game import Game
     game = Game(testRun=True)
     avgFps = game.run()
 
     assert avgFps > 30
+
+def test_memory():
+    game = Game(testRun=True)
+    avgFps = game.run()
+
+    process = psutil.Process()
+    mem = process.memory_info().rss/1024  # in bytes 
+    mem /= 1024
+
+    assert mem < 50
