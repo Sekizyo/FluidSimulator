68a14fca8a6fa12f29e1e7befeb8252cb7c20048
Matas Pieczulis <mateuszpieczulis123@outlook.com>
2023-08-02 09:26:21 +0200

    stable

--- modules/grid.py
+++ modules/grid.py
@@ -1,6 +1,7 @@
 import pygame
 import numpy
 from math import dist
+from random import randrange
 
 from modules import BLOCKSIZE, WIDTHBLOCKS, HEIGHTBLOCKS, FONT
 
@@ -35,25 +36,28 @@ class Moves(Position):
                 y1 = y+startPosY
 
                 if (0 <= x1 < WIDTHBLOCKS) and (0 <= y1 < HEIGHTBLOCKS):
-                    moves.append(self.blocks[y1][x1])
+                    moves.append((x1, y1))
         return moves
+    
+    def getValuesFromPos(self, pos):
+        values = []
+        for x, y in pos:
+            values.append(self.blocks[y][x])
+        return values
+    
+    def getAverageForList(self, list):
+        return sum(list) / len(list)    
 
 class Diffusion(Moves):
     def update(self, blocks):
         for y, col in enumerate(blocks):
             for x, block in enumerate(col):
                 if block >= 1:
-                    neighbours = self.getMoves((x, y), 2)
-                    avg = self.getAverageForBlocks(neighbours)
-                    if avg >= block:
-                        for neighbour in neighbours:
-                            neighbour = avg
-
-    def getAverageForBlocks(self, blocks):
-        avg = 0
-        for block in blocks:
-            avg += block
-        return avg / len(blocks)           
+                    neighboursPos = self.getMoves((x, y), 1)
+                    values = self.getValuesFromPos(neighboursPos)
+                    avg = self.getAverageForList(values)
+                    for x, y in neighboursPos:
+                        self.blocks[y][x] = avg
 
 class Render():
     def render(self, blockRect, blocks):
@@ -61,41 +65,51 @@ class Render():
             for x, rect in enumerate(col):
                 nparticles = blocks[y][x]
                 color = self.getColor(nparticles)
-                print(rect)
                 pygame.draw.rect(self.surface, color, rect)
 
     def getColor(self, nparticles):
-        return [100, 100, 100]
+        nrange = self.normalize(nparticles)
+        if nrange > 255:
+            return [255,255,255]
+        else:
+            return [nrange, 0, 0]
+    
+    def normalize(self, value):
+        old_value = value
+        old_min = 0
+        old_max = 1000
+        new_min = 0
+        new_max = 255
+
+        new_value = ( (old_value - old_min) / (old_max - old_min) ) * (new_max - new_min) + new_min
+        return int(new_value)
 
 class Grid(Render, Diffusion):
     def __init__(self, surface):
-        self.blocks = numpy.arange(WIDTHBLOCKS*HEIGHTBLOCKS).reshape(HEIGHTBLOCKS, WIDTHBLOCKS)
-        self.blockRect = numpy.arange(WIDTHBLOCKS*HEIGHTBLOCKS).reshape(HEIGHTBLOCKS, WIDTHBLOCKS)
         self.surface = surface
-        self.particleCount = 0
         self.size = BLOCKSIZE
+
+        self.blocks = []
+        self.blockRect = []
+        
+        self.particleCount = 0
         self.createBlocks()
 
     def addParticleToBlockByPos(self, mouse):
-        gridPos = self.getGridPosFromPos(mouse)
-        block = self.getBlockByGridPos(gridPos)
-        block.particles += 10000
+        x, y = self.getGridPosFromPos(mouse)
+        self.blocks[y][x] += 10000
         self.particleCount += 10000
 
     def createBlocks(self):
-        tempY = []
-        tempYRect = []
         for y in range(HEIGHTBLOCKS):
             tempX = []
             tempXRect = []
             for x in range(WIDTHBLOCKS):
                 tempX.append(0)
                 tempXRect.append(pygame.Rect(x*self.size, y*self.size, self.size, self.size))
-            tempY.append(tempX)
-            tempYRect.append(tempXRect)
-
-        self.blocks = tempY
-        self.blocksRect = tempYRect
+                
+            self.blocks.append(tempX)
+            self.blockRect.append(tempXRect)
 
     def renderGrid(self):
         self.render(self.blockRect, self.blocks)
@@ -104,6 +118,9 @@ class Grid(Render, Diffusion):
         self.update(self.blocks)
 
     def reset(self):
+        self.blocks = []
+        self.blocksRect = []
+
         self.particleCount = 0
-        for col in self.blocks:
-            col = []
+
+        self.createBlocks()
