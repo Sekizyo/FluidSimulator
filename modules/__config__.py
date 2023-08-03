a87686b2ee34f86cfc7af32b153606943dd62b8f
Matas Pieczulis <mateuszpieczulis123@outlook.com>
2023-08-03 14:47:42 +0200

    added coments to __config__

--- modules/__config__.py
+++ modules/__config__.py
@@ -1,13 +1,13 @@
 import pygame
 
-WIDTH = 1000
-HEIGHT = 1000
+WIDTH = 1000 #  - Screen width
+HEIGHT = 1000 #  - Screen height
 
-BLOCKSIZE = 10
+BLOCKSIZE = 10 #  - Scale block size, it greatly influences prerformance. Optimal values: 8 - 25
 
-DEPTH = 1
-VISCOSITY = 1
-PARTICLESPERCLICK = 10000
+DEPTH = 1 # - How far particle can spread. Optimal values: 1-2 
+VISCOSITY = 1 # - its resistance to deformation at a given rate. Lower value more likely to spread. Optimal values: 1-100
+PARTICLESPERCLICK = 10000 #  - Optimal values: depends on BLOCKSIZE
 
 WIDTHBLOCKS = WIDTH//BLOCKSIZE
 HEIGHTBLOCKS = HEIGHT//BLOCKSIZE
